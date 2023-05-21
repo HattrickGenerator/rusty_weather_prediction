73ea654deb19868622a2f16285b5c33804506705
michael <michaelgilch93@gmail.com>
2023-05-21 15:08:35 +0200

    try a working metar example. This is for north america however

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,58 @@
-use crate::forecast::forecast::prediction::Potatoe;
-
 pub mod forecast;
+use codespan_reporting::term::{
+    emit,
+    termcolor::{ColorChoice, StandardStream},
+};
+use std::io::Read;
+
+use weather_reports::parse::metar;
 
 fn main() {
-    let plant = Potatoe {};    
-    println!("I'm growing {:?}!", plant);
+    let filename : String = "/home/michael/git/rusty_weather_prediction/example_metar".to_string();
+
+    let report: String = match filename.as_ref() {
+        "-" => {
+            eprintln!("Reading from stdin");
+            let mut acc = String::default();
+            std::io::stdin().read_to_string(&mut acc).unwrap();
+            acc
+        }
+        filename => std::fs::read_to_string(&filename).expect("file isn't readable"),
+    };
+
+    if cfg!(feature = "trace") {
+        println!("[PEG_INPUT_START]");
+        println!("{}", report);
+        println!("[PEG_TRACE_START]");
+    }
+
+    match metar(&report) {
+        Ok(ast) => {
+            if !cfg!(feature = "trace") {
+                println!("{:#?}", ast);
+            }
+            eprintln!("Success!");
+        }
+        Err(err) => {
+            let mut writer = StandardStream::stderr(ColorChoice::Auto);
+            let config = codespan_reporting::term::Config::default();
+            emit(
+                &mut writer,
+                &config,
+                &codespan_reporting::files::SimpleFile::new(
+                    if filename == "-" {
+                        "<stdin>"
+                    } else {
+                        filename.as_str()
+                    },
+                    &report,
+                ),
+                &weather_reports::parse::into_diagnostic(&err),
+            )
+            .unwrap();
+        }
+    }
+    if cfg!(feature = "trace") {
+        println!("[PEG_TRACE_STOP]");
+    }
 }
