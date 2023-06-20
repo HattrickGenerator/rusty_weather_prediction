166f867e94f7e980b56dbc40018ade09d3196b7b
michael <michaelgilch93@gmail.com>
2023-06-20 21:57:47 +0200

    add the tokio environment for asyncs and print the status

--- src/main.rs
+++ src/main.rs
@@ -11,17 +11,16 @@ const LAT: f64 = 6.66;
 const LONG: f64 = 66.6;
 const TIME: u64 = 666;
 
-fn main() {
-    //code piece taken from:
-
+#[tokio::main]
+async fn main() {
     let api_key = "my_dark_sky_api_key"; // please don't actually hardcode your API key!
 
     let reqwest_client = Client::new();
-    let _api_client = ApiClient::new(&reqwest_client);
+    let api_client = ApiClient::new(&reqwest_client);
 
     let mut blocks = vec![ExcludeBlock::Daily, ExcludeBlock::Alerts];
 
-    let _forecast_request = ForecastRequestBuilder::new(api_key, LAT, LONG)
+    let forecast_request = ForecastRequestBuilder::new(api_key, LAT, LONG)
         .exclude_block(ExcludeBlock::Hourly)
         .exclude_blocks(&mut blocks)
         .extend(ExtendBy::Hourly)
@@ -29,13 +28,15 @@ fn main() {
         .units(Units::SI)
         .build();
 
-    let _time_machine_request = TimeMachineRequestBuilder::new(api_key, LAT, LONG, TIME)
+    let time_machine_request = TimeMachineRequestBuilder::new(api_key, LAT, LONG, TIME)
         .exclude_block(ExcludeBlock::Hourly)
         .exclude_blocks(&mut blocks)
         .lang(Lang::Arabic)
         .units(Units::Imperial)
         .build();
 
-    // let forecast_response = api_client.get_forecast(forecast_request).unwrap();
-    // let time_machine_response = api_client.get_time_machine(time_machine_request).unwrap();
+    let forecast_response = api_client.get_forecast(forecast_request).await.unwrap();
+    //let time_machine_response = api_client.get_time_machine(time_machine_request).await.unwrap();
+
+    println!("{}",forecast_response.status());
 }
